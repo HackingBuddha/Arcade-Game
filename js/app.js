caffeda55db814332c7009de08339a52ff7f0759
HackingBuddha <algis999@yahoo.com>
2015-11-19 18:46:11 +0200

    Update the Enemy class with position variables

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 200;
+    this.y = 300;
 };
 
 // Update the enemy's position, required method for game
@@ -34,7 +36,7 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.update = function() {
-    
+
 };
 
 // Now instantiate your objects.
